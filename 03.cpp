c8b60baab6091a99127b3f7291ee631457b72bf9
Gabriel Fischer <gabriel.fischer@outlook.de>
2023-12-03 18:06:42 +0100

    improved day 3

--- 03.cpp
+++ 03.cpp
@@ -8,64 +8,15 @@ bool is_symbol(char c) {
     return (not isdigit(c)) && (c != '.');
 }
 
-bool valid_index(int row, int col, int rows, int cols) {
-    return not(row < 0 || col < 0 || row >= rows || col >= cols);
-}
-
-bool is_part(const std::vector<std::vector<char>> &grid, std::size_t num_begin, std::size_t num_end, std::size_t row) {
-    bool is_part = false;
-    std::size_t n_rows = grid.size();
-    std::size_t n_columns = grid.at(0).size();
-
-    for (std::size_t col = num_begin; col < num_end; col++) {
-        if (valid_index(row - 1, col - 1, n_rows, n_columns))
-            if (is_symbol(grid.at(row - 1).at(col - 1)))
-                is_part = true;
-        if (valid_index(row - 1, col, n_rows, n_columns))
-            if (is_symbol(grid.at(row - 1).at(col)))
-                is_part = true;
-        if (valid_index(row - 1, col + 1, n_rows, n_columns))
-            if (is_symbol(grid.at(row - 1).at(col + 1)))
-                is_part = true;
-        if (valid_index(row, col - 1, n_rows, n_columns))
-            if (is_symbol(grid.at(row).at(col - 1)))
-                is_part = true;
-        if (valid_index(row, col + 1, n_rows, n_columns))
-            if (is_symbol(grid.at(row).at(col + 1)))
-                is_part = true;
-        if (valid_index(row + 1, col - 1, n_rows, n_columns))
-            if (is_symbol(grid.at(row + 1).at(col - 1)))
-                is_part = true;
-        if (valid_index(row + 1, col, n_rows, n_columns))
-            if (is_symbol(grid.at(row + 1).at(col)))
-                is_part = true;
-        if (valid_index(row + 1, col + 1, n_rows, n_columns))
-            if (is_symbol(grid.at(row + 1).at(col + 1)))
-                is_part = true;
-    }
-    return is_part;
-}
-
 //https://www.geeksforgeeks.org/find-all-adjacent-elements-of-given-element-in-a-2d-array-or-matrix/
 std::vector<std::tuple<int, int>> getAdjacent(std::vector<std::vector<std::tuple<int, int>>> &arr, int i, int j) {
-    // Size of given 2d array
     int n = arr.size();
     int m = arr[0].size();
 
-    // Initialising a vector array where
-    // adjacent elements will be stored
     std::vector<std::tuple<int, int>> v;
 
-    // Checking for adjacent elements
-    // and adding them to array
-
-    // Deviation of row that gets adjusted
-    // according to the provided position
-    for (int dx = (i > 0 ? -1 : 0); dx <= (i < n ? 1 : 0); ++dx) {
-        // Deviation of the column that
-        // gets adjusted according to
-        // the provided position
-        for (int dy = (j > 0 ? -1 : 0); dy <= (j < m ? 1 : 0); ++dy) {
+    for (int dx = (i > 0 ? -1 : 0); dx <= (i < n -1 ? 1 : 0); ++dx) {
+        for (int dy = (j > 0 ? -1 : 0); dy <= (j < m -1 ? 1 : 0); ++dy) {
             if (dx != 0 || dy != 0) {
                 v.push_back(arr[i + dx][j + dy]);
             }
@@ -76,7 +27,8 @@ std::vector<std::tuple<int, int>> getAdjacent(std::vector<std::vector<std::tuple
     return v;
 }
 
-u_int64_t second_solution(const std::vector<std::vector<char>> &grid) {
+u_int64_t second_solution(const std::vector<std::string> &grid) {
+
     std::size_t n_rows = grid.size();
     std::size_t n_columns = grid.at(0).size();
 
@@ -85,46 +37,21 @@ u_int64_t second_solution(const std::vector<std::vector<char>> &grid) {
     for (auto &row: numbers) {
         row.resize(n_columns);
     }
+    int id = 1;
 
-    std::size_t num_begin = 0;
-    std::size_t num_end = 0;
-    bool same_num = false;
-    int power = 1;
-    int num = 0;
-    int result = 0;
-    int id = 0;
-
+    u_int64_t result = 0;
     for (std::size_t row = 0; row < n_rows; row++) {
         for (std::size_t col = 0; col < n_columns; col++) {
-            if (auto c = grid.at(row).at(col); isdigit(c)) {
-                if (not same_num)
-                    num_begin = col;
-                num += power * (c - '0');
-                power *= 10;
-                same_num = true;
-            } else {
-                if (same_num) {
-                    num_end = col;
-                    for (std::size_t col_temp = num_begin; col_temp < num_end; col_temp++) {
-                        numbers.at(row).at(col_temp) = {num, id};
-                    }
-                    id++;
+            if ( isdigit(grid.at(row).at(col))) {
+                std::size_t length = 0;
+                int num = std::stoi(grid.at(row).substr(col), &length);
+                for(std::size_t i = 0;i<length;i++){
+                    numbers.at(row).at(col + i) = {num, id};
                 }
-                power = 1;
-                same_num = false;
-                num = 0;
+                id++;
+                col += length;
             }
         }
-        if (same_num) {
-            num_end = n_columns;
-            for (std::size_t col_temp = num_begin; col_temp < num_end; col_temp++) {
-                numbers.at(row).at(col_temp) = {num, id};
-            }
-            id++;
-        }
-        power = 1;
-        same_num = false;
-        num = 0;
     }
 
     for (std::size_t row = 0; row < n_rows; row++) {
@@ -132,9 +59,9 @@ u_int64_t second_solution(const std::vector<std::vector<char>> &grid) {
             if (grid.at(row).at(col) == '*') {
                 std::vector<std::tuple<int, int>> adj = getAdjacent(numbers, row, col);
                 auto unique_gears = adj
-                                    |ranges::views::unique([](std::tuple<int, int> t, std::tuple<int, int> s) { return get<1>(t) == get<1>(s); })
-                                    |ranges::views::filter([](std::tuple<int,int>t){return get<1>(t)!=0;});
-                if (ranges::distance(unique_gears) == 2) {
+                                    |ranges::views::filter([](std::tuple<int,int>t){return get<1>(t)!=0;})
+                                    |ranges::views::unique([](std::tuple<int, int> t, std::tuple<int, int> s) { return get<1>(t) == get<1>(s); });
+                if(ranges::distance(unique_gears) == 2) {
                     result+= ranges::fold_left(
                                 unique_gears
                                     | ranges::views::transform([](std::tuple<int, int> t) { return get<0>(t); })
@@ -149,61 +76,42 @@ u_int64_t second_solution(const std::vector<std::vector<char>> &grid) {
 }
 
 
-u_int64_t first_solution(const std::vector<std::vector<char>> &grid) {
+u_int64_t first_solution(const std::vector<std::string> &grid) {
+
 
     std::size_t n_rows = grid.size();
     std::size_t n_columns = grid.at(0).size();
 
-    std::size_t num_begin = 0;
-    std::size_t num_end = 0;
-    bool same_num = false;
-    int power = 1;
-    int num = 0;
-    int result = 0;
-
+    u_int64_t result = 0;
     for (std::size_t row = 0; row < n_rows; row++) {
         for (std::size_t col = 0; col < n_columns; col++) {
-            if (auto c = grid.at(row).at(col); isdigit(c)) {
-                if (not same_num)
-                    num_begin = col;
-                num += power * (c - '0');
-                power *= 10;
-                same_num = true;
-            } else {
-                if (same_num) {
-                    num_end = col;
-                    if (is_part(grid, num_begin, num_end, row)) {
-                        result += num;
+            if ( isdigit(grid.at(row).at(col))) {
+                std::size_t length = 0;
+                int num = std::stoi(grid.at(row).substr(col), &length);
+                bool valid = false;
+                for (std::size_t row_test{(row == 0) ? 0 : row - 1}; row_test <= std::min(row + 1, grid.size() - 1); ++row_test) {
+                    for (std::size_t col_test{(col == 0) ? 0 : col - 1}; col_test <= std::min(col + length, grid[row].size() - 1); ++col_test) {
+                        if (is_symbol(grid[row_test][col_test])) {
+                            valid = true;
+                        }
                     }
                 }
-                power = 1;
-                same_num = false;
-                num = 0;
+                if (valid) {
+                    result += num;
+                }
+                col += length;
             }
         }
-        if (same_num) {
-            num_end = n_columns;
-            if (is_part(grid, num_begin, num_end, row)) {
-                result += num;
-            }
-        }
-        power = 1;
-        same_num = false;
-        num = 0;
+
     }
     return result;
 }
 
 int main() {
     auto strings = ParserHelper::readLinesFromFile("../Input/03.txt");
-    std::vector<std::vector<char>> grid;
-    grid.reserve(strings.size());
-    for (std::string s: strings) {
-        grid.emplace_back(s.rbegin(), s.rend());
-    }
 
-    fmt::print("Solution 1: {}\n", first_solution(grid));
-    fmt::print("Solution 2: {}\n", second_solution(grid));
+    fmt::print("Solution 1: {}\n", first_solution(strings));
+    fmt::print("Solution 2: {}\n", second_solution(strings));
 
     return 0;
 }
